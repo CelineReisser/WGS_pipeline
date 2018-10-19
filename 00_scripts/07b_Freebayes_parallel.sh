452814ff26bc7f5cd1190f431a70519583e425f2
cmor2207 <celine.reisser@gmail.com>
2018-10-18 15:23:44 -1000

    Update 07b_Freebayes_parallel.sh

--- 00_scripts/07b_Freebayes_parallel.sh
+++ 00_scripts/07b_Freebayes_parallel.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
-#PBS -q omp
+#PBS -q 
 #PBS -l walltime=120:00:00
-#PBS -l mem=115g
-#PBS -l ncpus=40
+#PBS -l mem= g
+#PBS -l ncpus=
 #PBS -N freebayes
-#PBS -o /home1/datawork/creisser/sex_det/98_log_files/07b_freebayes_parallel_out.txt
+#PBS -o 98_log_files/07b_freebayes_parallel_out.txt
 
-DATADIRECTORY=/home1/scratch/creisser/sex_det/06_MD
-OUTDIR=/home1/scratch/creisser/sex_det/07_freebayes
-FREEBAYESENV=". /appli/bioinfo/freebayes/latest/env.sh"
-REF=/home1/datawork/creisser/sex_det/01_info_files/sspace.final.scaffolds.fasta
-INDEX=/home1/datawork/creisser/sex_det/01_info_files/sspace.final.scaffolds.fasta.fai
+DATADIRECTORY=/Path/to/your/copy/of/WGS_pipeline
+INDIR= 06_MD
+OUTDIR= 07_freebayes
+FREEBAYESENV= executables
+REF= 01_info_files/your_genome.fasta
+INDEX= 01_info_files/your_genome.fasta.fai
 
 $FREEBAYESENV
-mkdir -p $OUTDIR
-cd $DATADIRECTORY
 
-LS="ls $DATADIRECTORY/*.sorted.MD.bam"
-$LS > /home1/datawork/creisser/sex_det/00_scripts/bam_list_freebayes.txt
-BAM=/home1/datawork/creisser/sex_det/00_scripts/bam_list_freebayes.txt
+cd $DATADIRECTORY
+mkdir -p $OUTDIR
+
+LS="ls $DATADIRECTORY/$INDIR/*.sorted.MD.bam"
+$LS > 00_scripts/bam_list_freebayes.txt
+BAM=00_scripts/bam_list_freebayes.txt
 
 NCPU=28
 nAlleles=4
 
 freebayes-parallel  <(fasta_generate_regions.py $INDEX 100000) "$NCPU" \
--f $REF --use-best-n-alleles $nAlleles -C 5 --genotype-qualities -L $BAM > $OUTDIR/sex_det_snp.vcf
+-f $DATADIRECTORY/$REF --use-best-n-alleles $nAlleles -C 5 --genotype-qualities -L $DATADIRECTORY/$BAM > $DATADIRECTORY/$OUTDIR/sex_det_snp.vcf
 
 

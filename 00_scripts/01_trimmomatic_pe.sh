13f6ba4d6e3b73d43f6676588578e050da9bc2e6
cmor2207 <celine.reisser@gmail.com>
2018-10-18 14:54:35 -1000

    Update 01_trimmomatic_pe.sh

--- 00_scripts/01_trimmomatic_pe.sh
+++ 00_scripts/01_trimmomatic_pe.sh
@@ -1,11 +1,12 @@
-DATADIRECTORY= Path/to/your/copy/of/WGS_pipeline
-DATAINPUT= 02_data
-DATAOUTPUT= 03_trimmed
-SCRIPT= 00_scripts/01_trimmamatic_pe
-HEADER= 00_scripts/header.txt
-TRIMMOMATICENV=" executables"
+DATADIRECTORY=/Path/to/your/copy/of/WGS_pipeline
+DATAINPUT=02_data
+DATAOUTPUT=03_trimmed
+SCRIPT=00_scripts/01_trimmomatic_pe
+HEADER=00_scripts/header.txt
+TRIMMOMATICENV= executables
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
-LOG_FOLDER= 98_log_files
+LOG_FOLDER=98_log_files
+#BASE=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/00_scripts/base.txt
 NAME='cat 00_scripts/base.txt'
 
 
@@ -13,24 +14,23 @@ NAME='cat 00_scripts/base.txt'
 
 #>Illumina_TruSeq_LT_R1 AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC
 #>Illumina_TruSeq_LT_R2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT
-ADAPTERFILE= "00_scripts/adapters.fasta"
+ADAPTERFILE=00_scripts/adapters.fasta
 NCPU=4
 
 cd $DATADIRECTORY
 
 mkdir -p $SCRIPT
 mkdir -p $DATAOUTPUT
-
 for file in $($NAME)
 do
-         cp $HEADER $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
-         echo "cd $DATADIRECTORY" >> $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
-         echo "$TRIMMOMATICENV"  >> $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+         cp $HEADER $DATADIRECTORY$SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+         echo "cd $DATADIRECTORY" >> $DATADIRECTORY$SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+         echo "$TRIMMOMATICENV"  >> $DATADIRECTORY$SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
          echo "trimmomatic PE -Xmx23G \
         -threads 4 \
         -phred33 \
-        $DATADIRECTORY/"$file"_R1.fastq.gz \
-        $DATADIRECTORY/"$file"_R2.fastq.gz \
+        $DATAINPUT/"$file"_R1.fastq.gz \
+        $DATAINPUT/"$file"_R2.fastq.gz \
         $DATAOUTPUT/"$file"_R1.paired.fastq.gz \
         $DATAOUTPUT/"$file"_R1.single.fastq.gz \
         $DATAOUTPUT/"$file"_R2.paired.fastq.gz \
@@ -39,9 +39,7 @@ do
         LEADING:28 \
         TRAILING:28 \
         SLIDINGWINDOW:24:28 \
-        MINLEN:40 2> $LOG_FOLDER/log.trimmomatic.pe."$TIMESTAMP"" >> $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
-        qsub $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+        MINLEN:40 2> $LOG_FOLDER/log.trimmomatic.pe."$TIMESTAMP"" >> $DATADIRECTORY$SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+        qsub $DATADIRECTORY$SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
  
 done ;
-
-

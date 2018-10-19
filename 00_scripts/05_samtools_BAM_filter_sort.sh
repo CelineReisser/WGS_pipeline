c38d331d0d26efb2e068a13e9f9c84d12b384aba
cmor2207 <celine.reisser@gmail.com>
2018-10-18 15:06:12 -1000

    Update 05_samtools_BAM_filter_sort.sh

--- 00_scripts/05_samtools_BAM_filter_sort.sh
+++ 00_scripts/05_samtools_BAM_filter_sort.sh
@@ -1,23 +1,25 @@
-DATADIRECTORY=/home1/scratch/creisser/sex_det/04_mapped
-DATAOUTPUT=/home1/datawork/creisser/sex_det/05_filtered_BAM
-SCRIPT=/home1/datawork/creisser/sex_det/00_scripts/05_samtools_filter_sort
-HEADER=/home1/datawork/creisser/sex_det/00_scripts/header-big-mem.txt
-SAMTOOLSENV=". /appli/bioinfo/samtools/1.4.1/env.sh"
-NAME="cat /home1/datawork/creisser/sex_det/00_scripts/base.txt"
+DATADIRECTORY=/Path/to/your/copy/of/WGS_pipeline
+DATAINPUT=04_mapped
+DATAOUTPUT=05_filtered_BAM
+SCRIPT= 00_scripts/05_samtools_filter_sort
+HEADER= 00_scripts/header-big-mem.txt
+SAMTOOLSENV= executables
+NAME="cat 00_scripts/base.txt"
 
+cd $DATADIRECTORY
 mkdir -p $SCRIPT
 mkdir -p $DATAOUTPUT
 
 for FILE in $($NAME)
 do
-        cp $HEADER $SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "#PBS -o /home1/datawork/creisser/sex_det/98_log_files/05_samtools_filter_"$FILE".txt" >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "#PBS -N 05_samtools_filter_"$FILE" " >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-        echo "cd $DATADIRECTORY" >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-        echo "$SAMTOOLSENV"  >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-        echo "samtools view -b -f0x2 -F 256 -q5 "$FILE".sam -o $DATAOUTPUT/"$FILE".paired.bam"  >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "samtools sort -@ 8 $DATAOUTPUT/"$FILE".paired.bam -o $DATAOUTPUT/"$FILE".paired.sorted.bam"  >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-	echo "samtools index -b $DATAOUTPUT/"$FILE".paired.sorted.bam" >> $SCRIPT/samtools_${FILE##*/}.qsub ; 
-	echo "rm $DATAOUTPUT/"$FILE".paired.bam" >> $SCRIPT/samtools_${FILE##*/}.qsub ;
-        qsub $SCRIPT/samtools_${FILE##*/}.qsub ;
+        cp $DATADIRECTORY/$HEADER $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
+	echo "#PBS -o cd "$DATADIRECTORY"/98_log_files/05_samtools_filter_"$FILE".txt" >> $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
+	echo "#PBS -N 05_samtools_filter_"$FILE" " >> $DATADIRECTORY$SCRIPT/samtools_${FILE##*/}.qsub ;
+        echo "cd $DATAINPUT" >> $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
+        echo "$SAMTOOLSENV"  >> $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
+        echo "samtools view -b -f0x2 -F 256 -q5 "$FILE".sam -o $DATAOUTPUT/"$FILE".paired.bam"  >> $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
+	echo "samtools sort -@ 8 $DATADIRECTORY/$DATAOUTPUT/"$FILE".paired.bam -o $DATADIRECTORY/$DATAOUTPUT/"$FILE".paired.sorted.bam"  >> $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
+	echo "samtools index -b $DATADIRECTORY/$DATAOUTPUT/"$FILE".paired.sorted.bam" >> $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ; 
+	echo "rm $DATADIRECTORY/$DATAOUTPUT/"$FILE".paired.bam" >> $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
+        qsub $DATADIRECTORY/$SCRIPT/samtools_${FILE##*/}.qsub ;
 done ;

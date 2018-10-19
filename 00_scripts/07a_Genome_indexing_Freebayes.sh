5fe612971a4fb6d51dcd0f74b7bdba3425906890
cmor2207 <celine.reisser@gmail.com>
2018-10-18 15:11:49 -1000

    Update 07a_Genome_indexing_Freebayes.sh

--- 00_scripts/07a_Genome_indexing_Freebayes.sh
+++ 00_scripts/07a_Genome_indexing_Freebayes.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #PBS -N db1_BWA_indexing
-#PBS -o /home1/datawork/creisser/sex_det/98_log_files/07a_Genome_indexing_Freebayes.out
+#PBS -o 98_log_files/07a_Genome_indexing_Freebayes.log
 #PBS -l walltime=20:00:00
 #PBS -l mem=60g
 #PBS -r n
 
 # Global variables
-SAMTOOLSENV=". /appli/bioinfo/samtools/1.4.1/env.sh"
-BWA=/home1/datahome/creisser/local-programs/bwa-0.7.15
-FASTA=/home1/datawork/creisser/sex_det/01_info_files/sspace.final.scaffolds.fasta
+SAMTOOLSENV= executables
+BWA= exectuable/folder
+DATADIRECTORY=/Path/to/your/copy/of/WGS_pipeline
+FASTA=01_info_files/your_genome.fasta
 
 $SAMTOOLSENV
 
 # index the transcriptome
-samtools faidx $FASTA
+samtools faidx $DATADIRECTORY/$FASTA
 

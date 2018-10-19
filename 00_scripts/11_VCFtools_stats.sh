e6200bc6371904d9ca5d4dba30e44fd7805b560f
cmor2207 <celine.reisser@gmail.com>
2018-10-18 15:30:59 -1000

    Update 11_VCFtools_stats.sh

--- 00_scripts/11_VCFtools_stats.sh
+++ 00_scripts/11_VCFtools_stats.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
-#PBS -q mpi
+#PBS -q 
 #PBS -l walltime=24:00:00
 #PBS -l select=1:ncpus=28:mem=115g
-#PBS -o /home1/datahome/creisser/scratch/platax/sex_det/rna-seq_mapping_workflow/98_log_files
+#PBS -o 98_log_files
 
-DATADIRECTORY=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/
-INDIR=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/07_freebayes
-HEADER=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/00_scripts/header-big-mem.txt
-VCFTOOLSENV=". /appli/bioinfo/vcftools/0.1.14/env.sh"
+DATADIRECTORY=/Path/to/your/copy/of/WGS_pipeline
+INDIR=07_freebayes
+HEADER=00_scripts/header-big-mem.txt
+VCFTOOLSENV= executables
 
 $VCFTOOLSENV
-cd $INDIR
+cd $DATADIRECTORY/$INDIR
 
-vcftools --weir-fst-pop $DATADIRECTORY/00_scripts/base-M.txt --weir-fst-pop $DATADIRECTORY/00_scripts/base-F.txt --vcf $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic
+vcftools --weir-fst-pop $DATADIRECTORY/00_scripts/base-M.txt --weir-fst-pop $DATADIRECTORY/00_scripts/base-F.txt --vcf sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic
 
-#vcftools --TsTv --vcf $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic
+vcftools --relatedness --vcf sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out sex_det_snp_DP10_maf0.1_miss0.final_biallelic
 
-vcftools --relatedness --vcf $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic
+vcftools --depth --vcf sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out sex_det_snp_DP10_maf0.1_miss0.final_biallelic
 
-vcftools --depth --vcf $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic
-
-vcftools --geno-depth --vcf $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out $INDIR/sex_det_snp_DP10_maf0.1_miss0.final_biallelic
+vcftools --geno-depth --vcf sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf --out sex_det_snp_DP10_maf0.1_miss0.final_biallelic

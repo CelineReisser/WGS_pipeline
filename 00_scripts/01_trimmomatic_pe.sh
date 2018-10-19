882f88260a09082ab00da74bb29b68dcce67766e
cmor2207 <celine.reisser@gmail.com>
2018-10-18 14:16:09 -1000

    Update 01_trimmomatic_pe.sh

--- 00_scripts/01_trimmomatic_pe.sh
+++ 00_scripts/01_trimmomatic_pe.sh
@@ -1,40 +1,31 @@
-#!/usr/bin/env bash
-#PBS -N trimmomatic.sh
-#PBS -o /home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/98_log_files/trimmomatic.sh.out
-#PBS -q omp
-#PBS -l ncpus=4
-#PBS -l mem=60gb
-#PBS -l walltime=24:00:00
-#PBS -r n
-
-# Header
-DATADIRECTORY=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/02_data/raw
-DATAOUTPUT=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/03_trimmed
-SCRIPT=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/00_scripts/01_trimmomatic_pe.sh
-HEADER=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/00_scripts/header.txt
-TRIMMOMATICENV=". /appli/bioinfo/trimmomatic/latest/env.sh"
+DATADIRECTORY= #input directory
+DATAOUTPUT= #output directory
+SCRIPT= #path to store individual scripts
+HEADER=#path to header.txt for your cluster specifics
+TRIMMOMATICENV=" exectutables"
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
-LOG_FOLDER=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/98_log_files
-#BASE=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/00_scripts/base.txt
-NAME='cat /home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/00_scripts/base-4.txt'
-cd $PBS_O_WORKDIR
+LOG_FOLDER= #path to log folder
+NAME='cat /path/to/file/listing/sample_Names/base.txt'
+
 
 # Global variables
 
 #>Illumina_TruSeq_LT_R1 AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC
 #>Illumina_TruSeq_LT_R2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT
-ADAPTERFILE=/home1/scratch/creisser/platax/sex_det/rna-seq_mapping_workflow/00_scripts/adapters.fasta
+ADAPTERFILE="path to adapters.fasta"
 NCPU=4
 
-$TRIMMOMATICENV
+cd $DATADIRECTORY
 
-cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"
-
+mkdir -p $SCRIPT
+mkdir -p $DATAOUTPUT
 for file in $($NAME)
 do
-
-trimmomatic PE -Xmx23G \
-        -threads 8 \
+         cp $HEADER $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+         echo "cd $DATADIRECTORY" >> $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+         echo "$TRIMMOMATICENV"  >> $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+         echo "trimmomatic PE -Xmx23G \
+        -threads 4 \
         -phred33 \
         $DATADIRECTORY/"$file"_R1.fastq.gz \
         $DATADIRECTORY/"$file"_R2.fastq.gz \
@@ -46,6 +37,9 @@ trimmomatic PE -Xmx23G \
         LEADING:28 \
         TRAILING:28 \
         SLIDINGWINDOW:24:28 \
-        MINLEN:40 2> $LOG_FOLDER/log.trimmomatic.pe."$TIMESTAMP"
-    
-done
+        MINLEN:40 2> $LOG_FOLDER/log.trimmomatic.pe."$TIMESTAMP"" >> $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+        qsub $SCRIPT/trimmomatic_pe_${file##*/}.qsub ;
+ 
+done ;
+
+

23655800e80c050a237ed0b3790f7c14ebd1fdd6
cmor2207 <celine.reisser@gmail.com>
2018-10-18 15:27:32 -1000

    Update 09_VCFtools.sh

--- 00_scripts/09_VCFtools.sh
+++ 00_scripts/09_VCFtools.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-#PBS -q mpi
+#PBS -q 
 #PBS -l walltime=24:00:00
 #PBS -l select=1:ncpus=28:mem=115g
-#PBS -o /home1/datawork/creisser/sex_det/98_log_files
+#PBS -o 98_log_files
 #PBS -N vcftools
 
-DATADIRECTORY=/home1/scratch/creisser/sex_det/07_freebayes
+DATADIRECTORY=/Path/to/your/copy/of/WGS_pipeline
 INDIR=/home1/scratch/creisser/sex_det/07_freebayes
-VCFTOOLSENV=". /appli/bioinfo/vcftools/0.1.14/env.sh"
+VCFTOOLSENV= executables
 
 $VCFTOOLSENV
-cd $INDIR
+cd $DATADIRECTORY/$INDIR
 
-vcftools --maf 0.1 --max-missing 0.9 --vcf $INDIR/sex_det_snp_DP10.vcf --recode --out $INDIR/sex_det_snp_DP10_maf0.1_miss0.9
+vcftools --maf 0.1 --max-missing 0.9 --vcf sex_det_snp_DP10.vcf --recode --out sex_det_snp_DP10_maf0.1_miss0.9

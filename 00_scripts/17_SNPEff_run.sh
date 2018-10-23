b34fcea567fc958feafbe3b7dcb011720f7ba11c
cmor2207 <celine.reisser@gmail.com>
2018-10-22 14:39:43 -1000

    Create 17_SNPEff_run.sh

---
+++ 00_scripts/17_SNPEff_run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#PBS -q 
+#PBS -l walltime=
+#PBS -l select=1:ncpus=28:mem=115g
+#PBS -o 98_log_files
+#PBS -N snpeff
+
+SNPEFF=/Path/to/your/snpEff/folder
+DBNAME=your_genome
+VCF=07_freebayes/your_VCF.vcf
+
+OUTDIR=08_SNPEFF
+
+mkdir -p $OUTDIR
+cd $SNPEFF
+
+#Do the test for the dataset with no missing data:
+java -Xmx115G -jar $SNPEFF/snpEff.jar $DBNAME $VCF > $OUTDIR/your_VCF.ann.vcf &&
+mv $SNPEFF/snpEff_summary.html $OUTDIR/your_VCF.html
+mv $SNPEFF/snpEff_genes.txt $OUTDIR/your_VCF_genes_summary.txt

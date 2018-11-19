19c66a83dcc20a09511d14e9fcbec17afb32b64a
cmor2207 <celine.reisser@gmail.com>
2018-11-19 12:58:12 -1000

    Update 06_Mark_Filter_PCR_duplicates.sh

--- 00_scripts/06_Mark_Filter_PCR_duplicates.sh
+++ 00_scripts/06_Mark_Filter_PCR_duplicates.sh
@@ -15,9 +15,8 @@ do
         cp $HEADER $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub ;
 	echo "#PBS -o /home1/datawork/creisser/sex_det/98_log_files/picardMD_"$FILE".txt " >> $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub ;
 	echo "#PBS -N picardMD_"$FILE" " >> $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub ;
-	echo "module load java" >> $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub ;
+#	echo "module load java" >> $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub ;
         echo "cd $DATAINPUT" >> $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub ;
         echo "java -Xmx115G -jar ${PICARDTOOLS}/MarkDuplicates.jar I=./"$FILE".paired.sorted.bam O=$DATADIRECTORY/$OUTDIR/"$FILE".paired.sorted.MD.bam M=$DATADIRECTORY/$OUTDIR/"$FILE".MarkDup_metrics.txt ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=SILENT REMOVE_DUPLICATES=TRUE CREATE_INDEX=TRUE TMP_DIR=/home1/scratch/creisser/tmp/" >> $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub ;
-#        echo "rm "$FILE".paired.sorted.bam" >> $SCRIPT/picard_${FILE##*/}.qsub ;
         qsub $DATADIRECTORY/$SCRIPT/picard_${FILE##*/}.qsub;
 done;
